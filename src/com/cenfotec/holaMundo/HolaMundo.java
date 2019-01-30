70f4301323a03f37cef0f5ebc03b5660e0ebd380
Carolina Loaiza <karolina801chan@gmail.com>
2019-01-30 10:28:51 -0600

    Added base window with inputs (#1)
    * Added window with label and select
    
    * Added two languages

---
+++ src/com/cenfotec/holaMundo/HolaMundo.java
@@ -0,0 +1,65 @@
+package com.cenfotec.holaMundo;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+public class HolaMundo {
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HolaMundo window = new HolaMundo();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public HolaMundo() {
+		initialize();
+	}
+	
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 400, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		Font fontTitle = new Font("Verdana", Font.BOLD, 24);
+		JLabel lblTitle = new JLabel("Hola Mundo!");
+		lblTitle.setFont(fontTitle);
+		lblTitle.setBounds(20, 31, 200, 30);
+		frame.getContentPane().add(lblTitle);
+		
+		Font font = new Font("Verdana", Font.PLAIN, 17);
+		JLabel lblName = new JLabel("Seleccionar el idioma");
+		lblName.setFont(font);
+		lblName.setBounds(20, 70, 400, 30);
+		frame.getContentPane().add(lblName);
+		
+		JComboBox<String> comboBox = new JComboBox<String>();
+		for (String lag : Translate.getListaLenguajes()) {
+			comboBox.addItem(lag);
+		}
+		comboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		comboBox.setBounds(18, 110, 200, 20);
+		frame.getContentPane().add(comboBox);
+	}
+}
+
